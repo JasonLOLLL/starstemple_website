0b45a8ee2098c46e60075b4aa2c9994fa9612778
JasonLOLLL <71615537+JasonLOLLL@users.noreply.github.com>
2021-03-25 02:06:48 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Cache the DOM variables 
 const carousel_items = document.getElementsByClassName("carousel");
 const arrayLength = carousel_items.length;
 const carouselForwardButton = document.getElementById("carousel-forward-button");
@@ -8,46 +9,47 @@ const menuButton = document.getElementById("menu");
 const menuContainer = document.querySelector(".menu-container");
 const menuDiv = document.getElementById("menu-nav");
 
+// function to update the page number on the carousel
 updatePageNumber = () => {
     pageNumber.innerHTML = (currentItem+1) + " of " + arrayLength;
 }
 
-let currentItem = 0;
-carouselForwardButton.addEventListener("click", function() {
-    currentItem++;
-    if (typeof carousel_items[currentItem] != "undefined") {
+let currentItem = 0; // current item the carousel is on
+carouselForwardButton.addEventListener("click", function() { //for clicking forward
+    currentItem++; //increase by one
+    if (typeof carousel_items[currentItem] != "undefined") { //detects if there is a 'next' card and if there is, the hide the previous one and reveal the next card in the carousel
         carousel_items[currentItem - 1].classList.add("hidden");
         carousel_items[currentItem].classList.remove("hidden");
     } else {
-        carousel_items[currentItem - 1].classList.add("hidden");
-        currentItem = 0;
+        carousel_items[currentItem - 1].classList.add("hidden"); // if there is no next card, that means it reached the highest page and needs to return to 0
+        currentItem = 0; //back to the first card
         carousel_items[currentItem].classList.remove("hidden");
+    }
+    updatePageNumber(); //update page element
+})
+
+carouselBackwardButton.addEventListener("click", function () { //for clicking backward
+    currentItem--; //subtract one current item (going back one card)
+
+    if (typeof carousel_items[currentItem] != "undefined") { //if there is a card before the current one, then hide current one and reveal the last one
+        carousel_items[currentItem + 1].classList.add("hidden");
+        carousel_items[currentItem].classList.remove("hidden");
+    } else {
+        carousel_items[currentItem + 1].classList.add("hidden"); //if there is no card, it means it reached 0 and will return to the highest page
+        currentItem = arrayLength-1; //set current item to the highest page
+        carousel_items[currentItem].classList.remove("hidden"); 
     }
     updatePageNumber();
 })
 
-carouselBackwardButton.addEventListener("click", function () {
-    currentItem--;
-
-    if (typeof carousel_items[currentItem] != "undefined") {
-        carousel_items[currentItem + 1].classList.add("hidden");
-        carousel_items[currentItem].classList.remove("hidden");
-    } else {
-        carousel_items[currentItem + 1].classList.add("hidden");
-        currentItem = arrayLength-1;
-        carousel_items[currentItem].classList.remove("hidden");
-    }
-    updatePageNumber();
-})
-
-// Header fades into solid blue color when scrolling past home page
+// function > header fades into solid blue color when scrolling past home page
 window.addEventListener("scroll", function() {
     const header = document.querySelector(".wrapper");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 // Mobile menu
-
+// when menu icon is clicked, open the navigation menu
 menuButton.addEventListener("click", function() {
     if (menuContainer.style.display == "flex") {
         menuContainer.style.display = "none";
@@ -56,6 +58,7 @@ menuButton.addEventListener("click", function() {
     }
 });
 
+// if link is clicked on, then close the navigation menu
 menuClose = () => {
     menuContainer.style.display = "none";
 }
